0cc961f6905441a4e0e86dc0a7c209dee4d6ae59
karow <karow.foxx@gmail.com>
2020-11-16 17:40:27 +0000

    fixed float bug in position

--- Assets/Scripts/Packetman.cs
+++ Assets/Scripts/Packetman.cs
@@ -2,42 +2,43 @@
 using UnityEngine;
 
 public class Packetman : MonoBehaviour {
-    private float speed = 0.2f;
-    private Vector2 dest;
+    private const float Speed = 0.2f;
+    private Vector2 _dest;
 
-    private Collider2D collider;
+    private Collider2D _collider;
+    private Rigidbody2D _rigidBody;
 
     void Start() {
-        dest = transform.position;
-        collider = GetComponent<Collider2D>();
+        _dest = transform.position;
+        _collider = GetComponent<Collider2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate() {
-        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
-        GetComponent<Rigidbody2D>().MovePosition(p);
+    void Update() {
+        float roundx = (float)Math.Round(transform.position.x, 1);
+        float roundy = (float)Math.Round(transform.position.y, 1);
+        transform.position = new Vector3(roundx, roundy, transform.position.z);
 
         Vector2 currentPos = transform.position;
-        if (currentPos == dest) {
+        if (currentPos == _dest) {
             if (Input.GetKey(KeyCode.UpArrow) && canMove(Vector2.up)) {
-                dest = currentPos + Vector2.up;
+                _dest = currentPos + Vector2.up;
             }
             if (Input.GetKey(KeyCode.DownArrow) && canMove(Vector2.down)) {
-                dest = currentPos + Vector2.down;
+                _dest = currentPos + Vector2.down;
             }
             if (Input.GetKey(KeyCode.RightArrow) && canMove(Vector2.right)) {
-                dest = currentPos + Vector2.right;
+                _dest = currentPos + Vector2.right;
             }
             if (Input.GetKey(KeyCode.LeftArrow) && canMove(Vector2.left)) {
-                dest = currentPos + Vector2.left;
+                _dest = currentPos + Vector2.left;
             }
         }
+        _rigidBody.MovePosition(Vector2.MoveTowards(transform.position, _dest, Speed));
     }
 
     private bool canMove(Vector2 dir) {
         Vector2 pos = transform.position;
-        RaycastHit2D hit;
-        hit = Physics2D.Linecast(pos + dir, pos);
-        Debug.DrawLine(pos + dir, pos);
-        return (hit.collider == GetComponent<Collider2D>());
+        return Physics2D.Linecast(pos + dir, pos).collider == _collider;
     }
 }
