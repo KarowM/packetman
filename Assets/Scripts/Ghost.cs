f31fb01acef10242e6aeb1bbb902ba8da3548f04
karow <karow.foxx@gmail.com>
2020-12-03 17:21:03 +0000

    refactored ghost code

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 public class Ghost : MonoBehaviour {
     public Transform[] _route;
     private int _curPoint = 0;
+    
+    private Rigidbody2D _rigidBody;
+    private Animator _animator;
+
+    void Start() {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
 
     private float speed = 0.2f;
     void FixedUpdate() {
@@ -11,15 +19,15 @@ public class Ghost : MonoBehaviour {
             Vector2 movement = Vector2.MoveTowards(transform.position, 
                                              _route[_curPoint].position,
                                                    speed);
-            GetComponent<Rigidbody2D>().MovePosition(movement);
+            _rigidBody.MovePosition(movement);
         }
         else {
             _curPoint = (_curPoint + 1) % _route.Length;
         }
 
         Vector2 dir = _route[_curPoint].position - transform.position;
-        GetComponent<Animator>().SetFloat("DirX", dir.x);
-        GetComponent<Animator>().SetFloat("DirY", dir.y);
+        _animator.SetFloat("DirX", dir.x);
+        _animator.SetFloat("DirY", dir.y);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
