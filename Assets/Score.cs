ff97c66f7b5bccb7bbfa0c5e42e3dd1ce2ded0c1
karow <karow.foxx@gmail.com>
2020-12-04 18:55:44 +0000

    - added score to screen - increments on each packet pickup

---
+++ Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    private string _scorePrefix = "Score: ";
+    public Text scoreText;
+    public int score;
+
+    void Start() {
+    }
+
+    void Update() {
+        scoreText.text = _scorePrefix + score;
+    }
+
+    public void incrementScore() {
+        score++;
+    }
+}
--- Assets/Scripts/Packet.cs
+++ Assets/Scripts/Packet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Packet : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.name.Equals("packetman")) {
+            FindObjectOfType<Score>().incrementScore();
             Destroy(gameObject);
         }
     }
