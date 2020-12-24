488096b916e18a5cc45ae9375bd2498cb0e56868
karow <karow.foxx@gmail.com>
2020-12-24 14:37:35 +0000

    - added life manager to reduce lives when colliding with ghost

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LifeManager : MonoBehaviour {
+
+    private GameObject[] lives;
+    private int numLives;
+
+    private void Awake() {
+        if (FindObjectsOfType<LifeManager>().Length > 1) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start() {
+        numLives = 3;
+        lives = new GameObject[numLives];
+        lives[0] = GameObject.Find("Life 1");
+        lives[1] = GameObject.Find("Life 2");
+        lives[2] = GameObject.Find("Life 3");
+    }
+    public void PlayerDeath() {
+        if (numLives > 0) {
+            Destroy(lives[--numLives]);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        } else {
+            //TODO: go to fail screen
+            Debug.Log("Out of Lives!!!");
+        }
+    }
+}
--- Assets/Scripts/RedGhost.cs
+++ Assets/Scripts/RedGhost.cs
@@ -34,7 +34,7 @@ public class RedGhost : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.name.Equals("packetman")) {
-            Destroy(other.gameObject);
+            FindObjectOfType<LifeManager>().PlayerDeath();
         }
     }
 
