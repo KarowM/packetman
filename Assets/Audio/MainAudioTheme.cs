3ec2c8fff3c1176e3d6b87da09da91d1240c1093
karow <karow.foxx@gmail.com>
2020-12-24 17:54:01 +0000

    - added main audio track

---
+++ Assets/Audio/MainAudioTheme.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAudioTheme : MonoBehaviour {
+
+    private static MainAudioTheme instance = null;
+
+    public static MainAudioTheme Instance {
+        get { return instance;  }
+    }
+    void Awake()
+    {
+        if (instance !=null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
